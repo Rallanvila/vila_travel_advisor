9b6c47bab8acd7cd746f65deb0ae7401077c1e33
Allan Vila <allanvila@Allans-MBP.tampabay.rr.com>
2022-03-12 16:22:18 -0500

    finish cards

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { getPlacesData } from "./api";
 const App = () => {
 	const [places, setPlaces] = useState([]);
 	const [coordinates, setCoordinates] = useState({});
-	const [bounds, setBounds] = useState(null);
+	const [bounds, setBounds] = useState({});
 
 	useEffect(() => {
 		navigator.geolocation.getCurrentPosition(
@@ -21,11 +21,12 @@ const App = () => {
 	}, []);
 
 	useEffect(() => {
-		getPlacesData().then((data) => {
-			console.log(data);
+		console.log(bounds);
+		getPlacesData(bounds, bounds.sw, bounds.ne).then((data) => {
 			setPlaces(data);
+			// 	console.log(data);
 		});
-	}, [coordinates, bounds]);
+	}, [bounds, coordinates]);
 
 	return (
 		<div>
@@ -33,7 +34,7 @@ const App = () => {
 			<Header />
 			<Grid container spacing={3} style={{ width: "100%" }}>
 				<Grid item xs={12} md={4}>
-					<List />
+					<List places={places} />
 				</Grid>
 				<Grid item xs={12} md={8}>
 					<Map
--- src/api/index.js
+++ src/api/index.js
@@ -1,28 +1,32 @@
 import axios from "axios";
 
-const URL =
-	"https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+// const URL =
+// "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
 
-let options = {
-	params: {
-		bl_latitude: "11.847676",
-		tr_latitude: "12.838442",
-		bl_longitude: "109.095887",
-		tr_longitude: "109.149359",
-	},
-	headers: {
-		"x-rapidapi-host": "travel-advisor.p.rapidapi.com",
-		"x-rapidapi-key": "b08259437emsh73f96ea80105252p1ce92djsn719a5ce42bb8",
-	},
-};
-
-export const getPlacesData = async (sw, ne) => {
-	try {
-		const {
-			data: { data },
-		} = await axios.get(URL, options);
-		return data;
-	} catch (error) {
-		console.log(error);
+export const getPlacesData = async (bounds, ne, sw) => {
+	if (bounds != null) {
+		try {
+			const {
+				data: { data },
+			} = await axios.get(
+				"https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary",
+				{
+					params: {
+						bl_latitude: sw.lat,
+						tr_latitude: ne.lat,
+						bl_longitude: sw.lng,
+						tr_longitude: ne.lng,
+					},
+					headers: {
+						"x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+						"x-rapidapi-key":
+							"b08259437emsh73f96ea80105252p1ce92djsn719a5ce42bb8",
+					},
+				},
+			);
+			return data;
+		} catch (error) {
+			console.log(error);
+		}
 	}
 };
